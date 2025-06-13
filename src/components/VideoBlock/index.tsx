86696e391b64459bb8d0ba08b0e413fa6da47fa0
Luthfi <gdrfotolutfi@gmail.com>
2025-06-13 13:16:52 +0700

    finishing up website

--- src/components/VideoBlock/index.tsx
+++ src/components/VideoBlock/index.tsx
@@ -15,32 +15,32 @@ interface VideoBlockProps {
 
 const news = [
   {
-    date: "11 Jun 2025",
     title: "ChainSmart Launches SmartShield v1.1 with New Data Viewer Feature",
     content:
       "ChainSmart launched the SmartShield Data Integrity System with version 1.1 and added the feature of DataViewer.",
     image: "/img/svg/2.jpg",
+    linkUrl: "https://www.youtube.com/watch?v=I_Uaka30GUI" // Added link URL
   },
   {
-    date: "10 Jun 2025",
     title: "SmartShield Data Integrity System Launch v1.0",
     content:
       "ChainSmart launches the SmartShield Data Integrity System representing the culmination of over two years of enhancements to ChainSmart’s original blockchain based data integrity engine",
     image: "/img/svg/image2.jpg",
+    linkUrl: "https://www.youtube.com/watch?v=IFkj2zV8RX4" // Added link URL
   },
   {
-    date: "05 Jun 2025",
     title: "Discussion : SmartShield delivers data integrity through decentralization",
     content:
       "SmartShield, developed by ChainSmart, is a blockchain-based data integrity service that serves as a secure digital vault for critical information",
     image: "/img/svg/image3.jpg",
+    linkUrl: "https://www.youtube.com/watch?v=x6NRDt4qVTM" // Added link URL
   },
   {
-    date: "01 Jun 2025",
     title: "SmartShield Data Integrity System",
     content:
       "Think of SmartShield as a digital vault in which you can safely lock away a copy of your most valuable information. It delivers protection for your data through the power of decentalization.",
     image: "/img/svg/image1.jpg",
+    linkUrl: "https://www.youtube.com/watch?v=QEYgz4iK1A8" // Added link URL
   },
 ];
 
